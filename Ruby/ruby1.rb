51ae7473bb4cc17216dc3cada63b77fdd4e341fc
sumit <sumit459goyal@gmail.com>
2020-01-21 15:19:37 +0530

    Practice if,else,elsif,unless

---
+++ Ruby/ruby1.rb
@@ -0,0 +1,27 @@
+#If-Else
+
+=begin
+a,b=10,5
+if a < b
+  puts "#{a} is less than #{b}"
+ elsif a==b
+ 	puts"#{a} is equal to #{b}"
+else
+ 	puts "#{b} is less than #{a}"
+ end
+ 	
+Ans: beryl@beryl-To-be-filled-by-O-E-M:~/Desktop/Training/Ruby$ ruby ruby1.rb
+5 is less than 10
+
+
+x=11
+unless x>2
+	puts "X is less than 3"
+else 
+	puts "X is more than 2"
+end
+
+beryl@beryl-To-be-filled-by-O-E-M:~/Desktop/Training/Ruby$ ruby ruby1.rb
+X is more than 2
+
+=end
