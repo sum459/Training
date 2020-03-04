48b394c0c313776c424eb7a3f108616c415539d0
sumit <sumit459goyal@gmail.com>
2020-03-04 14:17:13 +0530

    Added root routes as user and improved flash messages

--- Rails/OBMA/config/routes.rb
+++ Rails/OBMA/config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   resources :books
-  get 'books/index'
-  get 'users/index'
-  get 'reviews/index'
+  
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
   get 'users/:id/is_delete', to:'users#is_delete', as: 'user_is_delete'
