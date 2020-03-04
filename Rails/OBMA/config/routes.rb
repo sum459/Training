064b72a3783614ce94a5f9d2d03a9b6cafe1908a
sumit <sumit459goyal@gmail.com>
2020-03-04 19:31:24 +0530

    Practiced and run ajax request

--- Rails/OBMA/config/routes.rb
+++ Rails/OBMA/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
-  get 'users/:id/is_delete', to:'users#is_delete', as: 'user_is_delete'
+  patch 'users/:id/is_delete', to:'users#is_delete', as: 'user_is_delete'
 
   resources :users do
   	resources :reviews
