673c1dce76b6dbfd9fd83202acdbe98d30ae4ee8
sumit <sumit459goyal@gmail.com>
2020-03-13 14:50:14 +0530

    Created sessions and login page in practice project

--- Rails/Practice/E-book_proj/config/routes.rb
+++ Rails/Practice/E-book_proj/config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
     
 	
 
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
 namespace 'api' do
 	namespace 'v1' do
 		resources :users
