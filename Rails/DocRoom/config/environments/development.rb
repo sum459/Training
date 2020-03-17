35d7f3acff06c53376ed575b7aec60452b5eccbd
sumit <sumit459goyal@gmail.com>
2020-03-17 15:03:15 +0530

    Created account on sendgrid and Added code for sending confirmation email

--- Rails/DocRoom/config/environments/development.rb
+++ Rails/DocRoom/config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 end
