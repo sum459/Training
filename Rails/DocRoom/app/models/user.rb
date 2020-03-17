b88e74060895458b77b4176d4bf8fc18566bdd28
sumit <sumit459goyal@gmail.com>
2020-03-17 16:34:59 +0530

    Added columns and validations in devise User

--- Rails/DocRoom/app/models/user.rb
+++ Rails/DocRoom/app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable#, :confirmable
 
   has_many :docs
+
+  validates_presence_of :first_name, :last_name, :user_name, :contact
+  validates_uniqueness_of :user_name, :contact
   
 end
--- Rails/DocRoom/db/schema.rb
+++ Rails/DocRoom/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_16_114851) do
+ActiveRecord::Schema.define(version: 2020_03_17_095824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2020_03_16_114851) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "user_name"
+    t.string "contact"
+    t.string "country"
+    t.string "state"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
