5de5f3ab995263be68da04acaa5a144347323a17
sumit goyal <sumit459goyal@gmail.com>
2020-03-24 16:31:20 +0530

    Generated scaffold of Cart and Line items and created record and upload images

--- Rails/soul sound/db/schema.rb
+++ Rails/soul sound/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_070220) do
+ActiveRecord::Schema.define(version: 2020_03_24_105404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
