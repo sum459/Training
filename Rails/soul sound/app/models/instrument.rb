d95bcab598209bab7559802dd6b6aeaee8005be1
sumit goyal <sumit459goyal@gmail.com>
2020-03-25 13:04:30 +0530

    Updated model and defined function and callback for line items

--- Rails/soul sound/app/models/instrument.rb
+++ Rails/soul sound/app/models/instrument.rb
@@ -1,8 +1,10 @@
 class Instrument < ApplicationRecord
- 
+  before_destroy :not_referenced_by_any_line_item
+  belongs_to :user, optional: true
+  has_many :line_items
 
   mount_uploader :image, ImageUploader
-  serialize :image, JSON 
+  serialize :image, JSON # If you use SQLite, add this line
 
   validates :title, :brand, :price, :model, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum aloud. "}
@@ -13,6 +15,13 @@ class Instrument < ApplicationRecord
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }
 
-  
+  private
+
+  def not_refereced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line items present")
+      throw :abort
+    end
+  end
 
 end
