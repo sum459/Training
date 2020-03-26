bb7ca2a9c17f9a1ef4fa7d1a1843dfccf86fcd4b
sumit goyal <sumit459goyal@gmail.com>
2020-03-26 15:55:56 +0530

    Added profile pic upload functionality on sign up and added bootstrap

--- Rails/DocRoom/app/helpers/user_helper.rb
+++ Rails/DocRoom/app/helpers/user_helper.rb
@@ -1,2 +1,12 @@
 module UserHelper
+	
+	def avatar_for(user, options = { size: 80})
+	   size = options[:size] 
+	   if user.avatar?
+	   	image_tag user.avatar.url(:thumb), width: size, class: 'avatar-image'
+	   else
+	   	image_tag 'def_pic.jpeg', width: size, class: 'avatar-image'
+	   end
+	end
+
 end
