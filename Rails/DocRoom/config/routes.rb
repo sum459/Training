4a2d193d502648f8ca2e0ea8b545213167b61a04
sumit goyal <sumit459goyal@gmail.com>
2020-03-26 13:17:14 +0530

    Updated routes, database, application layout in Docroom

--- Rails/DocRoom/config/routes.rb
+++ Rails/DocRoom/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'user/show'
+  get 'user/edit'
   devise_for :users
   get 'welcome/index'
   
   resources :docs
+  resources :users, only: [:show, :edit, :update]
   authenticated :user do
   	root "docs#index", as: "authenticated_root"
   end
