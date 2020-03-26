769e70a3e244d38cd7bfa477a0007a364783ce0d
sumit goyal <sumit459goyal@gmail.com>
2020-03-26 15:05:58 +0530

    Added avatar column in user

--- Rails/DocRoom/db/schema.rb
+++ Rails/DocRoom/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_095824) do
+ActiveRecord::Schema.define(version: 2020_03_26_083624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_095824) do
     t.string "country"
     t.string "state"
     t.string "city"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
