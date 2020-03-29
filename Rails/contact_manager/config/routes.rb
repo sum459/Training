7838d2b1575ed06e5e54192637d56c70094655a5
sumit goyal <sumit459goyal@gmail.com>
2020-03-29 21:32:23 +0530

    setup jquery and bootstrap

--- Rails/contact_manager/config/routes.rb
+++ Rails/contact_manager/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'contacts/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
