ca1b14ac651128408c3652f3a26accd5e1473a84
sumit goyal <sumit459goyal@gmail.com>
2020-03-29 22:04:22 +0530

    Added contact controller views and css

--- Rails/contact_manager/config/routes.rb
+++ Rails/contact_manager/config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'contacts/index'
-  get 'contacts/new'
-  get 'contacts/create'
-  get 'contacts/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts
+
+  root 'contacts#index'
 end
