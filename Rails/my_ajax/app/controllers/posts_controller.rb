9efb58bdd1683c54a9603ccd1412eb93b9229c19
sumit goyal <sumit459goyal@gmail.com>
2020-03-31 16:40:07 +0530

    Added video upload feature in posts and modify controllers,views,add video  uploader

--- Rails/my_ajax/app/controllers/posts_controller.rb
+++ Rails/my_ajax/app/controllers/posts_controller.rb
@@ -75,6 +75,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :avatar)
+      params.require(:post).permit(:title, :content, :avatar, :video)
     end
 end
--- Rails/my_ajax/app/models/post.rb
+++ Rails/my_ajax/app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
 
  mount_uploader :avatar, AvatarUploader
  serialize :avatar, JSON
-
+ mount_uploader :video, VideoUploader 
+ serialize :video, JSON 
 end
