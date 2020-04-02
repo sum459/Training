7881c5e1a0a5e68821c654170903a5083e7ba6f9
sumit goyal <sumit459goyal@gmail.com>
2020-04-02 12:47:52 +0530

    Updated javascript file

--- Rails/my_ajax/app/javascript/packs/application.js
+++ Rails/my_ajax/app/javascript/packs/application.js
@@ -17,3 +17,149 @@ require("jquery")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// video.addEventListener 'timeupdate', ((e) ->
+//   console.log(lengthWatched(e));
+//   return
+//   ), false
+
+// document.addEventListener('DOMContentLoaded',()=>{
+
+// // Get the video element with id="myVideo"
+// //  var vid = document.getElementById("myVideo");
+
+// // // Assign an ontimeupdate event to the video element, and execute a function if the current playback position has changed
+// //  vid.ontimeupdate = function() {myFunction()};
+// //  console.log(myFunction());
+
+// // function myFunction() {
+// //   // Display the current position of the video in a p element with id="demo"
+// //   document.getElementById("demo").innerHTML = vid.currentTime;
+// //   return(vid.duration);
+// // }
+
+// function init()
+// {
+//     var vid = document.getElementById("myVideo");
+//     vid.addEventListener("timeupdate", function(event){
+//         var result = vid.duration;
+//         // result is your return value
+//     }, false);
+
+// }
+// // // vid.addEventListener("timeupdate", );
+
+// // // setInterval(function(){
+// // //     percentComplete = vid.currentTime/vid.duration;
+// // // }, 300); 
+
+// // function myFunction(x,y) {
+ 
+// // var video = document.getElementById("myVideo");
+
+// // // // Attach a timeupdate event to the video element, and execute a function if the current playback position has changed
+// // // x.addEventListener("timeupdate", myFunction);
+
+// // // function myFunction() {
+// // //   // Display the current position of the video in a p element with id="demo"
+// // //   document.getElementById("demo").innerHTML = x.currentTime;
+
+// // //   console.log(x.currentTime);
+// // //   y = y + x.currentTime
+
+// // // }
+
+// // // console.log(y+'vvv')
+
+
+// // video.addEventListener('timeupdate', function() {
+// //   // don't have set the startTime yet? set it to our currentTime
+// //   if (!this._startTime) this._startTime = this.currentTime;
+
+// //   var playedTime = this.currentTime - this._startTime;
+
+// //   console.log(playedTime);
+
+// //   y = playedTime;
+// //   console.log(y+'xxx');
+// //   return y;
+  
+
+// // });
+
+// // video.addEventListener('seeking', function() {
+  
+// //   x = x + y ;
+// //   console.log(x + 'okk');
+
+
+// //   // reset the timeStart
+// //   this._startTime = undefined;
+
+  
+
+// // });
+
+// // console.log(x + 'done');
+
+
+
+// // };
+
+// // document.getElementById("demo").innerHTML = myFunction(0,0);
+
+// });
+
+
+//document.addEventListener('DOMContentLoaded',()=>{
+
+// var video = document.getElementById("myVideo");
+// var demo = document.getElementById("demo")
+
+// video.addEventListener("timeupdate", () => { getPlayedTime(video); });
+
+// //var y = video.addEventListener("timeupdate", () => { getPlayedTime(video); });
+
+// function getPlayedTime(video) {
+    
+//     var totalPlayed = 0;
+//     var played = video.played;
+
+//     for (var i = 0; i < played.length; i++) {
+//         totalPlayed += played.end(i) - played.start(i);
+//     }
+  
+    
+//     // return {
+
+//     //     total: totalPlayed,
+//     //     percent: totalPlayed / video.duration * 100
+
+//     // };
+//     demo.innerHTML = totalplayed ;
+
+//     //console.log(totalPlayed);
+// }
+$(document).ready(function(){
+	
+	$(".abc").on('click', GetContentAndSlide);
+    
+   // var id = $(this).attr("id");
+function  GetContentAndSlide(){
+   
+   duration = '10';
+
+    $.ajax({
+      url: "/posts/20",  
+      type: "put",
+      data: { post: { length: duration } },
+      dataType: "script"
+  
+});
+};
+});
+ 
+
+
+ 
+ 
--- Rails/my_ajax/db/schema.rb
+++ Rails/my_ajax/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_103518) do
+ActiveRecord::Schema.define(version: 2020_04_02_041212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_103518) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
     t.string "video"
+    t.string "length"
   end
 
 end
