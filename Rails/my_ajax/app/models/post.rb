ce83facc7e533db9fbe1559b20c9857e6e70ddca
sumit goyal <sumit459goyal@gmail.com>
2020-04-03 13:46:15 +0530

    Added User Admin Functionality and modified related fles

--- Rails/my_ajax/app/models/post.rb
+++ Rails/my_ajax/app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
 
+ belongs_to :user
+
  mount_uploader :avatar, AvatarUploader
  serialize :avatar, JSON
  mount_uploader :video, VideoUploader 
  serialize :video, JSON 
+
 end
