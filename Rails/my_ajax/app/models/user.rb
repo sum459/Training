481b975e8408f972d2db33ed37e08b74c579be96
sumit goyal <sumit459goyal@gmail.com>
2020-04-06 18:54:36 +0530

    customized user sign in via email/username and sign up fields

--- Rails/my_ajax/app/models/user.rb
+++ Rails/my_ajax/app/models/user.rb
@@ -1,9 +1,29 @@
 class User < ApplicationRecord
-has_many :posts
-acts_as_commontator
+
+ has_many :posts
+ acts_as_commontator
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:login]
 
+
+validates_presence_of :first_name, :last_name, :user_name, :contact
+  validates_uniqueness_of :user_name, :contact
+  
+  attr_writer :login
+
+  def login
+    @login || self.user_name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["user_name = :value Or email = :value", { :value => login }]).first
+      elsif conditions.has_key?(:user_name) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+  end
 end
