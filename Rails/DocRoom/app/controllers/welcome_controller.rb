418d7531bc28c4ea1c8bbbf84e96116949fdf247
sumit goyal <sumit459goyal@gmail.com>
2020-04-06 15:08:35 +0530

    Updated routes and views in DocRoom

--- Rails/DocRoom/app/controllers/welcome_controller.rb
+++ Rails/DocRoom/app/controllers/welcome_controller.rb
@@ -1,21 +1,4 @@
-# class WelcomeController < ApplicationController
-#   def index
-#   end
-# end
-
-# <header class="header_inner">
-#       <%= link_to "DocRoom", root_path, class: "logo" %>
-#       <nav>
-#         <% if user_signed_in? %>
-#         <%= link_to "Create Doc", new_doc_path, class: "logo" %>
-#         <%= link_to "Edit Profile", edit_user_registration_path, class: "logo" %>
-#         <%= link_to avatar_for(current_user, size: 40), current_user, class: "logo" %>
-#         <%= link_to "Sign Out", destroy_user_session_path, method: :delete, class: "logo" %>
-#         <% else %>
-#         <%= link_to "Log In", new_user_session_path, class: "logo" %>
-#         <% end %>
-#       </nav>
-#     </header>
-
-#       <p class="notice"><%= notice %></p>
-#       <p class="alert"><%= alert %></p>
+class WelcomeController < ApplicationController
+  def index
+  end
+end
--- Rails/DocRoom/config/routes.rb
+++ Rails/DocRoom/config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   
-   resources :chats
-  # get 'user/show'
-  # get 'user/edit'
-  # devise_for :users
-  # get 'welcome/index'
+  resources :chats
+  get 'user/show'
+  get 'user/edit'
+  devise_for :users
+  get 'welcome/index'
   
-  # resources :docs
-  # resources :users, only: [:show, :edit, :update]
-  # authenticated :user do
-  # 	root "docs#index", as: "authenticated_root"
-  # end
+  resources :docs
+  resources :users, only: [:show, :edit, :update]
+  authenticated :user do
+  	root "docs#index", as: "authenticated_root"
+  end
 
-  # root 'welcome#index'
-  
-  root 'chats#index'
+  root 'welcome#index'
 
 end
   
